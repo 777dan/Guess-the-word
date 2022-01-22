88d9031b4be694a5e8f3a073ba11849a834a8501
777dan <danya.nikitin7@gmail.com>
2022-01-22 11:46:26 +0200

    first commit

---
+++ word.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    let words = ["apple", "pear", "mouse", "house", "peach", "dog"];
+    let images = document.getElementsByTagName("img");
+    gen(words);
+    for (let i = 0; i < images.length; i++) {
+        images[i].onclick = checkWord;
+    }
+};
+
+function checkWord(event) {
+    let image = event.target;
+    let word = image.id;
+    alert(word);
+}
+
+function gen(words) {
+    for (let i = 0; i < 3; i++) {
+        let rand1 = Math.floor(Math.random() * words.length);
+        document.write(`<img src="images/${words[rand1]}.png" id="${words[rand1]}" width="300" height="300">`);
+        words.splice(rand1, 1);
+    }
+
+}
